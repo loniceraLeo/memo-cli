b2ff266bff28d7860d512ee90db6e8b49d7a4915
loniceraLeo <2192811348@qq.com>
2020-04-17 07:51:42 +0800

    add userconfig

---
+++ userconfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    host: '127.0.0.1',    
+    user: 'username',                
+    password: 'yourpwd',       
+    database: 'yourdatabase',
+    stringifyObject: true,
+}
+
+/**
+ * if you want to connect to the remote database, you must open 3306 port on your server
+ */
