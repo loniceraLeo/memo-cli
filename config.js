1f72e0ca8ab150bad0425aabd6ee89678b618d18
loniceraLeo <2192811348@qq.com>
2020-04-16 23:11:54 +0800

    upd

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    host: '173.82.202.5',    //localhost
-    user: 'lonicera',                //leo
-    password: 'W4maw4ma@',       //ap574218
-    database: 'blog',
-    stringifyObject: true,
-}
